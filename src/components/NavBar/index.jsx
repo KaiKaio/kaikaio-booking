77a4e29da15518665542ffa4234fee91b881e279
kaiwei <kaiwei@oxhainan.org>
2023-04-25 16:09:03 +0800

    适配IOS安全区域

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,42 +1,47 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types'
-import { TabBar } from 'zarm';
-import { useNavigate } from 'react-router-dom';
-import CustomIcon from '../CustomIcon';
-import s from './style.module.less';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { TabBar } from "zarm";
+import { useNavigate } from "react-router-dom";
+import CustomIcon from "../CustomIcon";
+import s from "./style.module.less";
 
 const NavBar = ({ showNav }) => {
-  const [activeKey, setActiveKey] = useState('/');
-  const navigateTo = useNavigate()
+  const [activeKey, setActiveKey] = useState("/");
+  const navigateTo = useNavigate();
 
   const chnageTab = (path) => {
-    setActiveKey(path)
-    navigateTo(path)
-  }
+    setActiveKey(path);
+    navigateTo(path);
+  };
 
   return (
-    <TabBar visible={showNav} className={s.tab} activeKey={activeKey} onChange={chnageTab}>
-        <TabBar.Item
-          itemKey="/"
-          title="账单"
-          icon={<CustomIcon type="icon-wj-zd" />}
-        />
-        <TabBar.Item
-          itemKey="/data"
-          title="统计"
-          icon={<CustomIcon type="icon-tongji" />}
-        />
-        <TabBar.Item
-          itemKey="/user"
-          title="我的"
-          icon={<CustomIcon type="icon-wode" />}
-        />
-      </TabBar>
+    <TabBar
+      visible={showNav}
+      className={`${s.tab} ${s.safeTab}`}
+      activeKey={activeKey}
+      onChange={chnageTab}
+    >
+      <TabBar.Item
+        itemKey="/"
+        title="账单"
+        icon={<CustomIcon type="icon-wj-zd" />}
+      />
+      <TabBar.Item
+        itemKey="/data"
+        title="统计"
+        icon={<CustomIcon type="icon-tongji" />}
+      />
+      <TabBar.Item
+        itemKey="/user"
+        title="我的"
+        icon={<CustomIcon type="icon-wode" />}
+      />
+    </TabBar>
   );
 };
 
 NavBar.propTypes = {
-  showNav: PropTypes.bool
-}
+  showNav: PropTypes.bool,
+};
 
 export default NavBar;
